7da1a2b5ff102733174fe8146587486671dd91c0
Zarifa Rasullu <zerife.resullu@gmail.com>
2021-06-16 17:22:50 +0400

    Last changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
+//variable declarations
 let container = document.querySelector(".box-container");
 let createBoxButton = document.querySelector(".createBox");
 let remove = document.querySelector(".remove");
-
-let num = document.querySelector("#num");
+let reset = document.querySelector("#reset").addEventListener("click", Reset);
+let colorChoice = document.querySelector("#colorChoice");
+let pixel = document.querySelector("#pixel");
 let color = "#000";
-let s = document.querySelector("span");
-
+let indicator = document.querySelector("span");
 let n;
-
+let eraser = document.querySelector("#eraser");
+let draw = document.querySelector("#draw");
+//function declarations
+function removeBoxes() {
+ if (container.children.length !== 0) {
+  container.innerHTML = "";
+ }
+}
 function createBox() {
- n = num.value;
- s.innerText = n;
+ n = pixel.value;
+ indicator.innerText = n;
 
  removeBoxes();
  for (let i = 0; i < n; i++) {
@@ -24,14 +32,6 @@ function createBox() {
   }
  }
 }
-function removeBoxes() {
- if (container.children.length !== 0) {
-  container.innerHTML = "";
- }
-}
-
-num.addEventListener("change", createBox);
-
 function Reset() {
  [...container.querySelectorAll(".box")].forEach((el) => {
   // el.classList.remove("red");
@@ -39,35 +39,23 @@ function Reset() {
   el.style["border-color"] = "white";
  });
 }
-let reset = document.querySelector("#reset").addEventListener("click", Reset);
-let colorChoice = document.querySelector("#colorChoice");
-colorChoice.addEventListener("touchmove", (e) => {
+
+//events
+pixel.addEventListener("change", createBox);
+
+colorChoice.addEventListener("change", (e) => {
  color = e.target.value;
 });
 
-function hexToRgbA(hex) {
- var c;
- if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-  c = hex.substring(1).split("");
-  if (c.length == 3) {
-   c = [c[0], c[0], c[1], c[1], c[2], c[2]];
-  }
-  c = "0x" + c.join("");
-  return "rgba(" + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(",") + ",1)";
- }
- throw new Error("Bad Hex");
-}
-// [...document.querySelector(".box-container").querySelectorAll(".box")].forEach(
-//  (el) => {
-//   console.log("hellds");
-//   el.addEventListener("click", () => {
-//    el.style["background-color"] = color;
-//    el.style["border-color"] = color;
-//    console.log("heldlo");
-//   });
-//  }
-// );
-container.addEventListener("mouseover", () => {
+eraser.addEventListener("click", () => {
+ [...container.querySelectorAll(".box")].forEach((el) => {
+  el.addEventListener("mouseover", () => {
+   el.style["background-color"] = "white";
+   el.style["border-color"] = "white";
+  });
+ });
+});
+draw.addEventListener("click", () => {
  [...container.querySelectorAll(".box")].forEach((el) => {
   el.addEventListener("mouseover", () => {
    el.style["background-color"] = color;
